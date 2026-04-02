43232ec57b3c8108b8568ff4806ae07128537e1b
田中亮 <r.tanaka@com-world.co.jp>
2026-04-02 11:35:32 +0900

    install cors dependency~

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-// ルートURL
-app.get("/", (req, res) => {
-  res.send("Hello Node.js + Express!");
+// 仮データ（DBの代わり）
+let todos = [
+  { id: 1, text: "勉強する" },
+  { id: 2, text: "買い物する" }
+];
+
+// 一覧取得
+app.get("/todos", (req, res) => {
+  res.json(todos);
 });
 
-// ポート3000で起動
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
