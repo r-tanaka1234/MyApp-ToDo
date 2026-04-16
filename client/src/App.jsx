f95343b1047f9b25ce5d685f1cc0543ba7c801fd
田中亮 <r.tanaka@com-world.co.jp>
2026-04-16 14:20:12 +0900

    画面にテキストボックス、登録ボタンを配置しToDoの登録を可能に

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [newText, setNewText] = useState("");
 
+  // 一覧取得
   useEffect(() => {
     fetch('http://localhost:3001/api/todos')
       .then(res => res.json())
@@ -15,10 +17,33 @@ function App() {
       });
   }, []);
 
+  // 
+  const addTodo = () => {
+    fetch('http://localhost:3001/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text: newText })
+    })
+      .then(res => res.json())
+      .then(data => {
+        // 画面に即反映
+        setTodos([...todos, data]);
+        setNewText(""); // 入力欄クリア
+      });
+  };
+
   return (
     <div>
       <h1>ToDo一覧</h1>
 
+      <input
+        type="text"
+        value={newText}
+        onChange={(e) => setNewText(e.target.value)}
+      />
+      <button onClick={addTodo}>追加</button>
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
