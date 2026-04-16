a51049800d5da335e8d6046478c9140406cc6b86
田中亮 <r.tanaka@com-world.co.jp>
2026-04-16 13:28:41 +0900

    CORSの対応を追加して、ReactからAPI通信を可能にした

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,16 +6,23 @@ function App() {
   useEffect(() => {
     fetch('http://localhost:3001/api/todos')
       .then(res => res.json())
-      .then(data => setTodos(data));
+      .then(data => {
+        console.log("取得データ:", data);
+        setTodos(data);
+      })
+      .catch(err => {
+        console.error("エラー:", err);
+      });
   }, []);
 
   return (
     <div>
       <h1>ToDo一覧</h1>
+
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
-            {todo.text} ({todo.completed ? "完了" : "未完了"})
+            {todo.text}（{todo.completed ? "完了" : "未完了"}）
           </li>
         ))}
       </ul>
